9707216e849a6ebfa01a7c73ea8aa2c6bde7a81e
Gurpal Singh <gillgurpal699@gmail.com>
2023-03-08 18:46:03 +0530

    use css module for index route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { clsx } from "clsx";
 export default function Home() {
   return (
     <>
@@ -11,10 +13,15 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="heroDiv w-screen h-screen flex flex-col justify-center items-center">
+      <div
+        className={clsx(
+          "w-screen h-screen flex flex-col justify-center items-center",
+          styles.heroDiv
+        )}
+      >
         <div className="w-[70%] text-center flex justify-center items-center flex-col">
           <h1 className="text-7xl text-gray-200 font-bold my-3">
-            Welcome to <span className="brandName">OurCode</span>
+            Welcome to <span className={styles.brandName}>OurCode</span>
           </h1>
           <p className="text-lg text-gray-400 text-center w-1/2">
             A place for you to find partners for personal projects, hackathons
